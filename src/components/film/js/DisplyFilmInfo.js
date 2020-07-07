0bce4c3459b959914be8411f4e41501e18719c20
malnaqeeb <mosleh.alnaqeeb@gmail.com>
2020-07-07 02:03:23 +0200

    last update

--- src/components/film/js/DisplyFilmInfo.js
+++ src/components/film/js/DisplyFilmInfo.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 // import { Input } from "semantic-ui-react";
 import { v4 as uuidv4 } from "uuid";
+import MoviesCover from "../../../PHOTO.json";
 import "../css/UserItem.css";
 import "../css/UsersList.css";
 import "../css/input.css";
@@ -8,6 +9,12 @@ import "../css/input.css";
 const FilmsCharacters = ({ filmcharacters, filmInfo }) => {
   const [input, setInput] = useState("");
   const [filterDisplay, setFilterDisplay] = useState(filmcharacters);
+
+  const cover = MoviesCover.filter((cover) => cover.name === filmInfo.title);
+  // const cover = MoviesCover.filter((cover) =>
+  //   cover.src.localeCompare(filmInfo.title)
+  // );
+  console.log("cover", cover);
 
   const handleChange = (e) => {
     setInput(e);
@@ -24,7 +31,9 @@ const FilmsCharacters = ({ filmcharacters, filmInfo }) => {
   return (
     <div className="movie-container">
       <div className="wrapper-header">
-        <div className="box1-header"></div>
+        <div className="box1-header">
+          <img src={cover[0].src} alt={filmInfo.title} />
+        </div>
         <div className="box2-header">
           <div className="box2-header-1">
             <h1>{filmInfo.title}</h1>
@@ -34,8 +43,6 @@ const FilmsCharacters = ({ filmcharacters, filmInfo }) => {
             {" "}
             <h3>Director:</h3>
             <p>{filmInfo.director}</p>
-            <h3>Producer:</h3>
-            <p>{filmInfo.producer}</p>
             <h3>Producer:</h3>
             <p>{filmInfo.producer}</p>
             <h3>Release Date</h3>
@@ -51,7 +58,7 @@ const FilmsCharacters = ({ filmcharacters, filmInfo }) => {
         />
       </div>
       <div>
-        <ul className="list-container">
+        <ul>
           {filterDisplay &&
             filterDisplay.map((character) => (
               <li key={uuidv4()} className="wrapper-li-content">
@@ -69,18 +76,18 @@ const FilmsCharacters = ({ filmcharacters, filmInfo }) => {
                 </div>
                 <div className="box box2" style={{ width: "100%" }}>
                   <div className="more-info">
-                    <h5>Birth_year:</h5> <span>{character.birth_year}</span>
+                    <h5>Birth Year:</h5> <span>{character.birth_year}</span>
                   </div>
                   <div className="more-info">
-                    <h5>eye_color:</h5>
+                    <h5>Eye Color:</h5>
                     <span>{character.eye_color}</span>
                   </div>
                   <div className="more-info">
-                    <h5>gender:</h5>
+                    <h5>Gender:</h5>
                     <span>{character.gender}</span>
                   </div>
                   <div className="more-info">
-                    <h5>hair_color:</h5>
+                    <h5>Hair Color:</h5>
                     <span>{character.hair_color}</span>
                   </div>
                 </div>
@@ -98,13 +105,13 @@ const FilmsCharacters = ({ filmcharacters, filmInfo }) => {
                     {character.starships.length > 0 ? (
                       character.starships.map((starship) => <p>{starship}</p>)
                     ) : (
-                      <h6>N/A</h6>
+                      <p>N/A</p>
                     )}
                     <h4>Vehicles:</h4>{" "}
                     {character.vehicles.length > 0 ? (
                       character.vehicles.map((vehicle) => <p>{vehicle}</p>)
                     ) : (
-                      <h6>N/A</h6>
+                      <p>N/A</p>
                     )}
                   </div>
                 </div>
